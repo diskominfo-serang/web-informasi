342fb868f2722ecdf0799f9cc2cbfb15585a000f
Yudi Faturohman <yudifaturohman198@gmail.com>
2020-02-26 15:12:00 +0700

    Ubah Hubungi Kami

--- include/section-hubungi-kami.php
+++ include/section-hubungi-kami.php
@@ -5,11 +5,46 @@
                 <!-- About Us Content -->
                 <div class="about-us-content bg-white mb-30 p-30 box-shadow">
                     <!-- Section Title -->
-                    <div class="section-heading">
-                        <h5>Hubungi Kami</h5>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur mauris id scelerisque eleifend. Nunc vestibulum cursus quam at scelerisque. Aliquam quis varius orci, vel tincidunt est. Proin ac tincidunti, atmots interdum erat. Maecenas neque lorem, aliquet in tempus non, efficitur ac neque.</p>
-                    <p>Phasellus elefend odio quis dolor pretium condimentu. Morbi quis mauris ipsum urna eu fermentum bentons Suspendisse auctor magna ac porta ornare. Fusce finibus nibh at lacinia lobortis.</p>
+                        <div class="section-heading">
+                            <h5>Hubungi Kami</h5>
+                        </div>
+
+                        <div class="contact-information mb-30">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur mauris id scelerisque eleifend. Nunc vestibulum cursus quam at scelerisque. Aliquam quis varius orci, vel tincidunt est. Proin ac tincidunti, atmots interdum erat. Maecenas neque lorem, aliquet in tempus non, efficitur ac neque.</p>
+
+                            <!-- Single Contact Info -->
+                            <div class="single-contact-info d-flex align-items-center">
+                                <div class="icon mr-15">
+                                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                </div>
+                                <div class="text">
+                                    <p>Alamat :</p>
+                                    <h6>Taman K3 Ciceri, Jl. Jend. Sudirman No.25, Banten 42118</h6>
+                                </div>
+                            </div>
+
+                            <!-- Single Contact Info -->
+                            <div class="single-contact-info d-flex align-items-center">
+                                <div class="icon mr-15">
+                                    <i class="fa fa-envelope" aria-hidden="true"></i>
+                                </div>
+                                <div class="text">
+                                    <p>Email :</p>
+                                    <h6>diskominfo@serangkab.go.id</h6>
+                                </div>
+                            </div>
+
+                            <!-- Single Contact Info -->
+                            <div class="single-contact-info d-flex align-items-center">
+                                <div class="icon mr-15">
+                                    <i class="fa fa-phone" aria-hidden="true"></i>
+                                </div>
+                                <div class="text">
+                                    <p>Telpon :</p>
+                                    <h6>(021) - 982738-2112</h6>
+                                </div>
+                            </div>
+                        </div>
                 </div>
             </div>
             <?php include 'section-rightbar.php'; ?>
